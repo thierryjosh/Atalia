ed7d90983648ab665a4cf984019d54600495beff
thierryjosh <thierry.corniere@arkho.net>
2024-06-09 19:45:14 +0200

    Add files via upload

---
+++ atalia.lisp
@@ -0,0 +1,16 @@
+;;;;initialization params
+;;;;
+(load "/home/arkho/quicklisp/setup.lisp")
+;(load "/home/arkho/.emacs.d/misc/cdatetime.lisp")
+;(ql:quickload "cl-smtp")
+;(ql:quickload "cl-mathstats")
+(load "/home/arkho/.emacs.d/david/graph.lisp")
+(load "/home/arkho/.emacs.d/heuristic/eew-scan.lisp")
+(load "/home/arkho/.emacs.d/heuristic/nlp-eng.lisp")
+(load "/home/arkho/.emacs.d/avatar05.lisp")
+(set-voice-active 1 1)
+(set-learning-lvl 1)
+(get-ginf)
+(terpri)
+(start-conversation)
+
