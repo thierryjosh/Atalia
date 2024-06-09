48512a1ba96a4107d7a6ea6f870b9588e336b92c
thierryjosh <thierry.corniere@arkho.net>
2024-06-09 18:12:07 +0200

    Add files via upload

---
+++ init.lisp
@@ -0,0 +1,40 @@
+;;;;initialization params
+;;;;
+;;;;     quicklisp
+;;;;
+(load "/home/arkho/quicklisp/setup.lisp")
+;;;;
+;;;;     extern packages
+;;;;
+;(ql:quickload :cl+ssl)
+(ql:quickload :cl-smtp)
+;(ql:quickload "cl-mathstats")
+(ql:quickload :cl-dbi)
+(ql:quickload :xmls)
+(ql:quickload :drakma)
+(ql:quickload :cl-pdf)
+(ql:quickload :cl-ppcre)
+(ql:quickload :plump)
+;;;;
+;;;;     spores -Atalia
+;;;;
+(load "/home/arkho/.emacs.d/david/graph.lisp")
+(load "/home/arkho/.emacs.d/heuristic/tcltk.lisp")
+(load "/home/arkho/.emacs.d/heuristic/sockets.lisp")
+(load "/home/arkho/.emacs.d/heuristic/genxml.lisp")
+(load "/home/arkho/.emacs.d/heuristic/entrez.lisp")
+(load "/home/arkho/.emacs.d/heuristic/genrdf.lisp")
+(load "/home/arkho/.emacs.d/heuristic/genpdf.lisp")
+(load "/home/arkho/.emacs.d/heuristic/genhtml.lisp")
+(load "/home/arkho/.emacs.d/heuristic/genoffi.lisp")
+(load "/home/arkho/.emacs.d/heuristic/document.lisp")
+(load "/home/arkho/.emacs.d/heuristic/eew-scan.lisp")
+(load "/home/arkho/.emacs.d/heuristic/nlp-eng.lisp")
+(load "/home/arkho/.emacs.d/heuristic/chess-core.lisp")
+(load "/home/arkho/.emacs.d/heuristic/chess-pgn.lisp")
+;;;;
+
+
+
+
+
